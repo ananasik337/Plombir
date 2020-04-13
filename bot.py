d02a4160b22dc69a268cbca6198d4f1630a2ec44
ananasik337 <55545186+ananasik337@users.noreply.github.com>
2020-04-13 09:59:13 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -188,17 +188,17 @@ async def Info(ctx, member: discord.Member = None):
 
 @Bot.command(pass_context= True)
 async def info(ctx, user: discord.User):
+    await ctx.message.delete()
     emb = discord.Embed(title= "Карточка",colour= 0xFF7F00)
     emb.add_field(name= "Имя", value= user.name)
     emb.add_field(name= "Зашел с", value= str(user.joined_at)[:16])
     emb.add_field(name= "Айди", value= user.id)
-    if user.game is not None:
-        emb.add_field(name= "Игра", value= user.game)
+    if user.activity is not None:
+        emb.add_field(name= "Игра", value= {user.activity})
     emb.set_thumbnail(url= user.avatar_url)
     emb.set_author(name= Bot.user.name, url= "ВОДОЛАЗ#1245")
     emb.set_footer(text= "Вызвано: {}".format(user.name), icon_url= user.avatar.url)
     await ctx.send(embed= emb)
-    await ctx.message.delete()
 
 token = os.environ.get('BOT_TOKEN')
 Bot.run(str(token))
