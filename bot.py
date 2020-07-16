65f1a73b557a7d7cddde0d008f157f8e77cd3e52
ananasik337 <55545186+ananasik337@users.noreply.github.com>
2020-07-16 15:42:49 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -166,7 +166,7 @@ async def ban(ctx, member : discord.Member, reason=None):
 
 #------------------------------------------------------------------------------------------------------------------------#
 
-@Bot.command()
+@Bot.command(name = "avatar","avatarka")
 async def аватар(ctx, member : discord.Member = None):
     user = ctx.message.author if (member == None) else member
     await ctx.message.delete()
